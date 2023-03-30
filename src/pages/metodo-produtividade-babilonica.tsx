6c312d711634852c3cef36465792b9bd390519c3
rafael <rcborges98@gmail.com>
2023-03-29 22:56:08 -0300

    feat: first commit

---
+++ src/pages/metodo-produtividade-babilonica.tsx
@@ -0,0 +1,21 @@
+export default function MetodoProdutividadeBabilonica() {
+
+  const externalLink = 'https://pay.kiwify.com.br/KsgR8E7' 
+
+  return (
+    <main>
+      <img className='desktop' src='foto-1.png'/> 
+
+      <div className='container-botao'>
+        <a href={externalLink}/>
+      </div>
+
+        <img className='desktop' src='foto-2.png'/> 
+
+      <div className='container-botao' >
+        <a href={externalLink}/>
+      </div>
+        <img className='desktop' src='foto-3.png'/> 
+    </main>
+  )
+}
