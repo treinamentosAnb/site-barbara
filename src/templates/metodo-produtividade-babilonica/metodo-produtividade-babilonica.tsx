c89ece88ae9825df53826ebc60d3df8e69889ee5
rafael <rcborges98@gmail.com>
2023-04-28 08:08:26 -0300

    chore: refactor the code

--- src/templates/metodo-produtividade-babilonica/metodo-produtividade-babilonica.tsx
+++ src/templates/metodo-produtividade-babilonica/metodo-produtividade-babilonica.tsx
@@ -1,33 +1,71 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 import Image from 'next/image'
+import { useState } from 'react'
 import { Faq } from './Faq/Faq'
 import { questionsAndAnswers } from './questionsAndAnswers'
 import * as S from './styled'
+// import VideoModal from './VideoModal'
 
 const MetodoProdutividadeBabilonica = () => {
   const externalLink = 'https://pay.kiwify.com.br/KsgR8E7'
+  const supportLink =
+    'https://wa.me/558499871905?text=Ol%C3%A1%21+Gostaria+de+tirar+algumas+d%C3%BAvidas+sobre+os+treinamentos+da+Babi.'
+
+  const [openModal, setOpenModal] = useState(false)
 
   return (
     <S.Container>
+      {/* <VideoModal isOpen={openModal} handleClose={() => setOpenModal(false)} /> */}
       <S.DesktopContent>
         <Image src="/desktop-1.png" alt={''} priority fill />
 
-        <S.ButtonContainer>
-          <S.FirstButton href={externalLink} />
-        </S.ButtonContainer>
+        <S.VideoContainer onClick={() => setOpenModal(true)} />
 
         <Image src="/desktop-2.png" alt={''} priority fill />
+        <S.ButtonContainer>
+          <S.Button href={externalLink} />
+        </S.ButtonContainer>
+
+        <Image src="/desktop-3.png" alt={''} priority fill />
+        <S.ButtonContainer>
+          <S.Button href={externalLink} />
+        </S.ButtonContainer>
+
+        <Image src="/desktop-4.png" alt={''} priority fill />
+        <S.ButtonContainer>
+          <S.BigButton href={externalLink} />
+        </S.ButtonContainer>
+
+        <Image src="/desktop-5.png" alt={''} priority fill />
+        <S.ButtonContainer>
+          <S.Button href={supportLink} />
+        </S.ButtonContainer>
       </S.DesktopContent>
 
       <S.MobileContent>
         <Image src="/mobile-1.png" alt={''} priority fill />
-
-        <S.ButtonContainer>
-          <S.FirstButton href={externalLink} />
-        </S.ButtonContainer>
+        <S.VideoContainer onClick={() => setOpenModal(true)} />
 
         <Image src="/mobile-2.png" alt={''} priority fill />
+        <S.ButtonContainer>
+          <S.Button href={externalLink} />
+        </S.ButtonContainer>
+
+        <Image src="/mobile-3.png" alt={''} priority fill />
+        <S.ButtonContainer>
+          <S.Button href={externalLink} />
+        </S.ButtonContainer>
+
+        <Image src="/mobile-4.png" alt={''} priority fill />
+        <S.ButtonContainer>
+          <S.BigButton href={externalLink} />
+        </S.ButtonContainer>
+
+        <Image src="/mobile-5.png" alt={''} priority fill />
+        <S.ButtonContainer>
+          <S.BigButton href={supportLink} />
+        </S.ButtonContainer>
       </S.MobileContent>
 
       <S.QuestionsAndAnswersContainer>
@@ -35,6 +73,7 @@ const MetodoProdutividadeBabilonica = () => {
         {questionsAndAnswers.map((item, index) => (
           <Faq question={item.question} answer={item.answer} key={index} />
         ))}
+        <p> Bárbara Dolbeth 2023 © todos os direitos reservados</p>
       </S.QuestionsAndAnswersContainer>
     </S.Container>
   )
