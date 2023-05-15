850dbc972d81749efcb34dc7f659e6a10ff3dc62
rafael <rcborges98@gmail.com>
2023-05-15 16:25:18 -0300

    feat: add pixel script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,30 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '765872398436813');
+fbq('track', 'PageView');`
+          }}
+        />
+      </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=765872398436813&ev=PageView&noscript=1"
+/>`
+          }}
+        />
         <Main />
         <NextScript />
       </body>
